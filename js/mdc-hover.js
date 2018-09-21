21e1a7264caa1d940061d773936d3fc7ff7d3be2
lapprand <lapprand@gmail.com>
2018-09-20 21:20:33 -0300

    Change card elevation

--- js/mdc-hover.js
+++ js/mdc-hover.js
@@ -2,13 +2,11 @@ var hoverEls = document.querySelectorAll('#hover');
 
 hoverEls.forEach(function(hoverEl) {
   hoverEl.addEventListener('mouseenter', function () {
-    console.log("Entrou");
-    this.classList.remove('mdc-elevation--z2');
+    this.classList.remove('mdc-elevation--z1');
     this.classList.add('mdc-elevation--z8');
   });
   hoverEl.addEventListener('mouseleave', function () {
-    console.log("Saiu");
     this.classList.remove('mdc-elevation--z8');
-    this.classList.add('mdc-elevation--z2');
+    this.classList.add('mdc-elevation--z1');
   });
 });
