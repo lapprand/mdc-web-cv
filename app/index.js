0e2a02eaa348b8a40928f3bf6664eb998c26d779
lapprand <lapprand@gmail.com>
2018-09-23 19:02:31 -0300

    Add loader and hide content until load

--- app/index.js
+++ app/index.js
@@ -2,4 +2,6 @@ window.onload = _ => {
     require('./src/js/mdc-hover');
     require('./src/js/sliderconf');
     require('./src/js/animations');
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector(".fit-viewport").style.display = "grid";
 };
