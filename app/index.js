aeeddce85df580e5b42e83a59b0df8f82a1bb449
lapprand <lapprand@gmail.com>
2018-10-12 11:09:13 -0300

    Change loader and viewport wrapper to loader and content as ids

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 window.onload = _ => {
     require('./src/js/animations');
     require('./src/js/mdc-hover');
-    document.querySelector(".loader").style.display = "none";
-    document.querySelector(".fit-viewport").style.display = "block";
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("#content").style.display = "grid";
 };
