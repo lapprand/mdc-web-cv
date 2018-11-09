7aaf2bd8e2818f5f732f0a210690aeb79dff2346
lapprand <lapprand@gmail.com>
2018-11-08 23:17:32 -0300

    Remove unused code

--- app/src/js/scroll.js
+++ app/src/js/scroll.js
@@ -11,33 +11,14 @@ scrollIndicator.addEventListener('click', function () {
 });
 
 function checkScroll() {
-    // var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    // var scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-    // console.log(`${document.body.offsetHeight + scrollTop} < 50 && ${scrollTop} >= ${scrollHeight}`)
-    // if (scrollTop + getDocHeight() == scrollHeight) {
-    //     scrollIndicator.classList.remove('mdc-fab--exited');
-    // } else {
-    //     scrollIndicator.classList.add('mdc-fab--exited');
-    // }
-
     var scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
     var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
     var clientHeight = window.innerHeight;
-    console.log(scrollTop + "+" + clientHeight + " = " + (scrollTop + clientHeight) + " = " + scrollHeight);
     if (scrollTop + clientHeight >= scrollHeight) {
         scrollIndicator.classList.add('mdc-fab--exited');
     } else {
         scrollIndicator.classList.remove('mdc-fab--exited');
     }
-}
-
-function getDocHeight() {
-    var D = document;
-    return Math.max(
-        D.body.scrollHeight, D.documentElement.scrollHeight,
-        D.body.offsetHeight, D.documentElement.offsetHeight,
-        D.body.clientHeight, D.documentElement.clientHeight
-    );
 }
 
 function resizeChecks() {
